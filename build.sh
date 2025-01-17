6fe6d628e642f73d0f02bfc6ba2b8c51630cf104
Andrew Marcuse <fileformat@gmail.com>
2025-01-17 08:47:53 -0500

    Attempt to fix build problem on CF pages

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ echo "INFO: build starting at $(date -u +%Y-%m-%dT%H:%M:%SZ)"
 echo "INFO: Jekyll location = $(which jekyll)"
 echo "INFO: Jekyll version = $(jekyll --version)"
 
-jekyll build --source docs
+bundle exec jekyll build --source docs
 
 echo "INFO: build complete at $(date -u +%Y-%m-%dT%H:%M:%SZ)"
